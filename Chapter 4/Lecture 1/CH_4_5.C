aea6bac2aec0f15f555030d778dbb7a0f932fa2d
Fenil-Patel3 <128152158+Fenil-Patel3@users.noreply.github.com>
2023-03-31 18:21:23 +0530

    Add files via upload

---
+++ Chapter 4/Lecture 1/CH_4_5.C
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int x,y,z;
+	clrscr();
+	printf("enter the value of x:");
+	scanf("%d",&x);
+	printf("enter the value of y:");
+	scanf("%d",&y);
+	printf("enter the value of z:");
+	scanf("%d",&z);
+	printf("%d",(x*x*x)-(y*y*y)-(z*z*z)-(3*x*x*y)-(3*x*x*z)+(3*x*y*y)-(3*y*y*z)+(3*x*z*z)-(3*y*z*z)+(6*x*y*z));
+	getch();
+}
