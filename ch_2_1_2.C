08b425ccac378fce2bec18071b6fd27d68222c35
Fenil-Patel3 <fenilpatel3366@gmail.com>
2023-03-31 18:06:14 +0530

    Ch_2_lec_1

---
+++ ch_2_1_2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   int a=57;
+   int b=20;
+   int c;
+   clrscr();
+   c=a-b;
+   printf("the sum of a and b;%d",c);
+   getch();
+
+}
---
+++ ch_2_1_5.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   int a=33;
+   int b=100;
+   int c;
+   clrscr();
+   c=a%b;
+   printf("the sum of a and b;%d",c);
+   getch();
+
+}
